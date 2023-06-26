623ad12c3db174c77b50303a3ad35d0ad2ad3b22
Tatyana Kapina <deniscapin@yandex.ru>
2023-06-26 18:23:53 +0300

    Classwork 1_2

--- Lesson_1/1_1/Program.cs
+++ Lesson_1/1_1/Program.cs
@@ -1,8 +1,8 @@
 ﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет,
 // является ли первое число квадратом второго.
-System.Console.WriteLine("Введите первое число: ");
+System.Console.WriteLine("Введите число: ");
 int a = int.Parse(Console.ReadLine()!);
-System.Console.WriteLine("Введите второе число: ");
+System.Console.WriteLine("Введите число: ");
 int b = int.Parse(Console.ReadLine()!);
 
 if (a / b == b)
---
+++ Lesson_1/1_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// 2. Напишите программу, которая будет выдавать название дня
+// недели по заданному номеру.
+
+string a = Console.ReadLine()!;
+switch (a)
+{
+    case "1":
+    Console.Write("Понедельнтк");
+    break;
+    case "2":
+    Console.Write("Вторник");
+    break;
+    case "3":
+    Console.Write("Среда");
+    break;
+    case "4":
+    Console.Write("Четверг");
+    break;
+    case "5":
+    Console.Write("Пятница");
+    break;
+    case "6":
+    Console.Write("Суббота");
+    break;
+    case "7":
+    Console.Write("Воскресенье");
+    break;
+
+    default:
+    Console.Write("Такого дня недели нет");
+    break;
+}
