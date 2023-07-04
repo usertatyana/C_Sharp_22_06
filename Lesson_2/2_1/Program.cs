1ea3bd3470da41d1f55c2f1738a37c3992a68d1e
Tatyana Kapina <deniscapin@yandex.ru>
2023-07-04 13:14:57 +0300

    Classwork 2_1

---
+++ Lesson_2/2_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// 1. Напишите программу, которая выводит случайное число
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int Max(int num)
+{
+    if (num / 10 > num % 10)
+        return num / 10;
+
+    return num % 10;
+
+}
+int RNum = new Random().Next(10, 100);
+Console.WriteLine("" + RNum);
+int result = Max(RNum);
+Console.WriteLine("" + result);
